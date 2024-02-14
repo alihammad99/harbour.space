4d1a941ee397482cdead29917af37042bf99fd47
Ali Hammad <aliressan.ar@gmail.com>
2024-02-14 15:44:17 +0300

    add space to partner's name

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -43,7 +43,7 @@ export default component$(({ data }: { data: any }) => {
           />
 
           <p class={styles.partnerText}>
-            Powered by:{" "}
+            Powered by:
             <span class={styles.partnerName}>{data.company.name}</span>
           </p>
         </div>
@@ -80,5 +80,5 @@ const styles = {
   patternImage:
     "invisible absolute bottom-0 right-0 z-[-1] w-[100rem] translate-x-[20%] translate-y-[20%] lg:visible",
   partnerText: "flex font-light text-slate-500 lg:flex-col",
-  partnerName: "text-lg text-slate-600",
+  partnerName: "text-lg pl-1 lg:p-0 text-slate-600",
 };
