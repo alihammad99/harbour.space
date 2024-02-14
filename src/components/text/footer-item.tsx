1823fb63371b5e9d16f4d27f0504f2846ad2063e
Ali Hammad <aliressan.ar@gmail.com>
2024-02-14 14:31:23 +0300

    sticky footer - font updated

--- src/components/text/footer-item.tsx
+++ src/components/text/footer-item.tsx
@@ -4,8 +4,8 @@ export default component$(
   ({ title, value }: { title: string; value: string }) => {
     return (
       <li>
-        <p class="m-0 text-sm font-semibold text-slate-700">{title}</p>
-        <p class="m-0 text-sm font-thin text-slate-700">{value}</p>
+        <p class="m-0 text-sm font-medium text-slate-700">{title}</p>
+        <p class="m-0 text-sm font-light text-slate-700">{value}</p>
       </li>
     );
   },
--- src/sections/footer/index.tsx
+++ src/sections/footer/index.tsx
@@ -4,7 +4,7 @@ import FooterItem from "~/components/text/footer-item";
 export default component$(() => {
   return (
     <ul class="fixed bottom-0 left-0 z-[100] hidden w-full justify-center gap-20 border-t border-slate-300 bg-white py-4 lg:flex">
-      <FooterItem title="Zeptolab" value="Marketing Performance" />
+      <FooterItem title="Zeptolab" value="Data Science" />
       <FooterItem title="Location" value="Barcelona" />
       <FooterItem title="Duration" value="1 Year Full-Time" />
       <FooterItem title="Start date" value="30 Nov 2020" />
